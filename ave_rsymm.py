a45fd0ebb40ce1f13d56b33b099f09faea2250c3
David Schaich <dschaich@syr.edu>
2014-05-19 19:22:08 -0400

    Update a couple of scripts to optionally consider smeared measurements, set up basic effective mass calculation.

--- ave_rsymm.py
+++ ave_rsymm.py
@@ -4,7 +4,7 @@ import sys
 import glob
 import numpy as np
 # ------------------------------------------------------------------
-# Print blocked Konishi and SUGRA correlator averages
+# Print blocked plaquette and R-symmetry-modified Wilson loop averages
 # with blocked standard errors
 
 # Parse arguments: first is thermalization cut,
@@ -43,32 +43,18 @@ if len(cfgs) == 0:
 # If we're missing some initial measurements,
 # increase thermalization cut
 cut = cfgs[0]
-
-# Extract lattice volume from path
-# For now only Nt is used; we assume it is a two-digit number!!!
-path = os.getcwd()
-if 'Gauge' in path:   # Tom's runs
-  temp = path.split('Gauge')
-  L = int(temp[1][:1])      # First digit after 'Gauge'
-  Nt = int(temp[1][1:3])    # Second and third digits after 'nt'
-else:                 # My runs
-  temp = path.split('nt')
-  L = int((temp[0].split('_'))[-1])     # Everything between '_' and 'nt'
-  Nt = int(temp[1][:2])                 # First two digits after 'nt'
-
-# We exploit t <--> Nt - t symmetry
-# To only print 0 through Nt / 2 (a total of Npts points)
-Npts = Nt / 2 + 1  # Assume Nt is even
 # ------------------------------------------------------------------
 
 
 
 # ------------------------------------------------------------------
-# Construct arrays of blocked measurements for each correlator
-# For now just average over all 25 SUGRA components
-# K = Konishi, S = SUGRA
-Kdat = [[] for x in range(Npts)]
-Sdat = [[] for x in range(Npts)]
+# Construct arrays of blocked measurements for each observable
+# P = usual plaquette, F = modified loop
+# D = difference (F - P), R = ratio (F - P)/(F + P)
+Pdat = []
+Fdat = []
+Ddat = []
+Rdat = []
 
 # Monitor block lengths, starting and ending MDTU
 block_data = [[], [], []]
@@ -76,57 +62,78 @@ count = 0         # How many measurements in each block
 begin = cut       # Where each block begins, to be incremented
 
 # Accumulators
-tK = [0 for x in range(Npts)]
-tS = [0 for x in range(Npts)]
+tP = 0.0
+tF = 0.0
+tD = 0.0
+tR = 0.0
 for MDTU in cfgs:
   # If we're done with this block, record it and reset for the next
   if MDTU >= (begin + block_size):
-    for t in range(Npts):
-      Kdat[t].append(tK[t] / float(count))
-      Sdat[t].append(tS[t] / float(25. * count))  # Average over all
+    Pdat.append(tP / float(count))
+    Fdat.append(tF / float(count))
+    Ddat.append(tD / float(count))
+    Rdat.append(tR / float(count))
     # Record and reset block data
     block_data[0].append(count)
     count = 0
     block_data[1].append(begin)
     begin += block_size
     block_data[2].append(begin)
-    tK = [0 for x in range(Npts)]
-    tS = [0 for x in range(Npts)]
+    tP = 0.0
+    tF = 0.0
+    tD = 0.0
+    tR = 0.0
 
   # Running averages
+  dat = 0.0
   filename = 'Out/' + tag + '.' + str(MDTU)
   toOpen = glob.glob(filename)
   if len(toOpen) > 1:
     print "ERROR: multiple files named %s:" % filename,
     print toOpen
   for line in open(toOpen[0]):
-    # Format: KONISHI t dat
-    if line.startswith('KONISHI '):
+    # Format: GMES Re(Poly) Im(Poly) cg_iters ss_plaq st_plaq S_B
+    # Average over space--space and space--time plaquettes
+    # Until 17 January 2014, the plaquettes didn't involve the diagonal link
+    if line.startswith('GMES '):
       temp = line.split()
-      time = int(temp[1])
-      if time == 0:
-        count += 1    # Only increment once per measurement!
-      dat = float(temp[2])
-      tK[time] += dat
-    # Format: SUGRA a b t dat
-    elif line.startswith('SUGRA '):
+      sav = (float(temp[4]) + float(temp[5])) / 2.0
+      tP += sav
+      count += 1    # Only increment once per measurement!
+    # If we have smeared the configuration
+    # we need to overwrite sav to pick up the smeared plaquette!
+    elif line.startswith('AFTER  MIN_PLAQ '):
       temp = line.split()
-      time = int(temp[3])
-      dat = float(temp[4])
-      tS[time] += dat
+      tP -= sav
+      sav = (float(temp[3]) + float(temp[4])) / 2.0
+      tP += sav
+    # Format: FLAVOR a b dat
+    # This comes last in the output files,
+    # so it will use the correct sav
+    elif line.startswith('FLAVOR '):
+      temp = line.split()
+      dat += float(temp[3])
+      # Average before comparing to plaquette, after this last one
+      if int(temp[1]) == 4 and int(temp[2]) == 3:
+        dat += float(temp[3])
+        dat /= 20.0
+        tF += dat
+        tD += (dat - sav)
+        tR += (dat - sav) / (dat + sav)
 
 # Check special case that last block is full
 # Assume last few measurements are equally spaced
 if cfgs[-1] >= (begin + block_size - cfgs[-1] + cfgs[-2]):
-  for t in range(Npts):
-    Kdat[t].append(tK[t] / float(count))
-    Sdat[t].append(tS[t] / float(25. * count))  # Average over all
+  Pdat.append(tP / float(count))
+  Fdat.append(tF / float(count))
+  Ddat.append(tD / float(count))
+  Rdat.append(tR / float(count))
   # Record block data
   block_data[0].append(count)
   block_data[1].append(begin)
   block_data[2].append(begin + block_size)
 
-Nblocks = len(Kdat[0])
+Nblocks = len(Pdat)
 # ------------------------------------------------------------------
 
 
@@ -138,29 +145,34 @@ if Nblocks == 1:
   sys.exit(1)
 
 print "Averaging with %d blocks of length %d MDTU" % (Nblocks, block_size)
-Kfile = open('results/konishi.dat', 'w')
-print >> Kfile, "# Averaging with %d blocks of length %d MDTU" % (Nblocks, block_size)
-Sfile = open('results/sugra.dat', 'w')
-print >> Sfile, "# Averaging with %d blocks of length %d MDTU" % (Nblocks, block_size)
-for t in range(Npts):
-  # Konishi
-  dat = np.array(Kdat[t])
-  ave = np.mean(dat, dtype = np.float64)
-  err = np.std(dat, dtype = np.float64) / np.sqrt(Nblocks - 1.)
-  print >> Kfile, "%d %.6g %.4g # %d" % (t, ave, err, Nblocks)
+outfile = open('results/flavor.dat', 'w')
+print >> outfile, "# Averaging with %d blocks of length %d MDTU" % (Nblocks, block_size)
+print >> outfile, "# diff err rel err flavor err plaq err"
 
-  # SUGRA
-  dat = np.array(Sdat[t])
-  ave = np.mean(dat, dtype = np.float64)
-  err = np.std(dat, dtype = np.float64) / np.sqrt(Nblocks - 1.)
-  print >> Sfile, "%d %.6g %.4g # %d" % (t, ave, err, Nblocks)
+dat = np.array(Ddat)
+ave = np.mean(dat, dtype = np.float64)
+err = np.std(dat, dtype = np.float64) / np.sqrt(Nblocks - 1.)
+print >> outfile, "%.6g %.4g" % (ave, err),
+
+dat = np.array(Rdat)
+ave = np.mean(dat, dtype = np.float64)
+err = np.std(dat, dtype = np.float64) / np.sqrt(Nblocks - 1.)
+print >> outfile, "%.6g %.4g" % (ave, err),
+
+dat = np.array(Fdat)
+ave = np.mean(dat, dtype = np.float64)
+err = np.std(dat, dtype = np.float64) / np.sqrt(Nblocks - 1.)
+print >> outfile, "%.6g %.4g" % (ave, err),
+
+dat = np.array(Pdat)
+ave = np.mean(dat, dtype = np.float64)
+err = np.std(dat, dtype = np.float64) / np.sqrt(Nblocks - 1.)
+print >> outfile, "%.6g %.4g # %d" % (ave, err, Nblocks)
 
 # More detailed block information
 #for i in range(Nblocks):
-#  for outfile in [Kfile, Sfile]:
-#    print >> outfile, \
-#          "# Block %2d has %d measurements from MDTU in [%d, %d)" \
-#          % (i + 1, block_data[0][i], block_data[1][i], block_data[2][i])
-Kfile.close()
-Sfile.close()
+#  print >> outfile, \
+#        "# Block %2d has %d measurements from MDTU in [%d, %d)" \
+#        % (i + 1, block_data[0][i], block_data[1][i], block_data[2][i])
+outfile.close()
 # ------------------------------------------------------------------
--- sugra_effm.py
+++ sugra_effm.py
@@ -4,17 +4,18 @@ import sys
 import glob
 import numpy as np
 # ------------------------------------------------------------------
-# Print blocked plaquette and R-symmetry-modified Wilson loop averages
-# with blocked standard errors
+# Compute SUGRA correlator effective masses, using a blocked jackknife
 
 # Parse arguments: first is thermalization cut,
 # second is block size (should be larger than autocorrelation time)
 # We discard any partial blocks at the end
-if len(sys.argv) < 3:
-  print "Usage:", str(sys.argv[0]), "<cut> <block>"
+# Third argument tells us whether to analyze "corr" or "stout" files
+if len(sys.argv) < 4:
+  print "Usage:", str(sys.argv[0]), "<cut> <block> <tag>"
   sys.exit(1)
 cut = int(sys.argv[1])
 block_size = int(sys.argv[2])
+tag = str(sys.argv[3])
 # ------------------------------------------------------------------
 
 
@@ -27,31 +28,45 @@ if not os.path.isdir('Out'):
 
 # Construct list of which configurations have been analyzed
 cfgs = []
-for filename in glob.glob('Out/corr.*'):
+files = 'Out/' + tag + '.*'
+for filename in glob.glob(files):
   cfg = int(filename.split('.')[-1])    # Number after last .
   if cfg not in cfgs and cfg > cut:
     cfgs.append(cfg)
 cfgs.sort()
 
 if len(cfgs) == 0:
-  print "ERROR: no files Out/corr.* found"
+  print "ERROR: no files", files, "found"
   sys.exit(1)
 
 # If we're missing some initial measurements,
 # increase thermalization cut
 cut = cfgs[0]
+
+# Extract lattice volume from path
+# For now only Nt is used; we assume it is a two-digit number!!!
+path = os.getcwd()
+if 'Gauge' in path:   # Tom's runs
+  temp = path.split('Gauge')
+  L = int(temp[1][:1])      # First digit after 'Gauge'
+  Nt = int(temp[1][1:3])    # Second and third digits after 'nt'
+else:                 # My runs
+  temp = path.split('nt')
+  L = int((temp[0].split('_'))[-1])     # Everything between '_' and 'nt'
+  Nt = int(temp[1][:2])                 # First two digits after 'nt'
+
+# We exploit t <--> Nt - t symmetry
+# To only print 0 through Nt / 2 (a total of Npts points)
+Npts = Nt / 2 + 1  # Assume Nt is even
 # ------------------------------------------------------------------
 
 
 
 # ------------------------------------------------------------------
-# Construct arrays of blocked measurements for each observable
-# P = usual plaquette, F = modified loop
-# D = difference (F - P), R = ratio (F - P)/(F + P)
-Pdat = []
-Fdat = []
-Ddat = []
-Rdat = []
+# Construct arrays of blocked measurements for each correlator
+# For now just average over all 25 SUGRA components
+# This constant factor will cancel out in the effective mass ratio
+effm = [[] for x in range(Npts - 1)]
 
 # Monitor block lengths, starting and ending MDTU
 block_data = [[], [], []]
@@ -59,69 +74,45 @@ count = 0         # How many measurements in each block
 begin = cut       # Where each block begins, to be incremented
 
 # Accumulators
-tP = 0.0
-tF = 0.0
-tD = 0.0
-tR = 0.0
+tM = [0 for x in range(Npts)]
 for MDTU in cfgs:
   # If we're done with this block, record it and reset for the next
   if MDTU >= (begin + block_size):
-    Pdat.append(tP / float(count))
-    Fdat.append(tF / float(count))
-    Ddat.append(tD / float(count))
-    Rdat.append(tR / float(count))
+    for t in range(Npts - 1):
+      effm[t].append(np.log(tM[t] / tM[t + 1]))
     # Record and reset block data
     block_data[0].append(count)
     count = 0
     block_data[1].append(begin)
     begin += block_size
     block_data[2].append(begin)
-    tP = 0.0
-    tF = 0.0
-    tD = 0.0
-    tR = 0.0
+    tM = [0 for x in range(Npts)]
 
   # Running averages
-  dat = 0.0
-  filename = 'Out/corr.' + str(MDTU)
+  filename = 'Out/' + tag + '.' + str(MDTU)
   toOpen = glob.glob(filename)
   if len(toOpen) > 1:
     print "ERROR: multiple files named %s:" % filename,
     print toOpen
   for line in open(toOpen[0]):
-    # Format: GMES Re(Poly) Im(Poly) cg_iters ss_plaq st_plaq S_B
-    # Average over space--space and space--time plaquettes
-    # Until 17 January 2014, the plaquettes didn't involve the diagonal link
-    if line.startswith('GMES '):
+    # Format: SUGRA a b t dat
+    if line.startswith('SUGRA '):
       temp = line.split()
-      sav = (float(temp[4]) + float(temp[5])) / 2.0
-      tP += sav
-      count += 1    # Only increment once per measurement!
-    # Format: FLAVOR a b dat
-    elif line.startswith('FLAVOR '):
-      temp = line.split()
-      dat += float(temp[3])
-      # Average before comparing to plaquette, after this last one
-      if int(temp[1]) == 4 and int(temp[2]) == 3:
-        dat += float(temp[3])
-        dat /= 20.0
-        tF += dat
-        tD += (dat - sav)
-        tR += (dat - sav) / (dat + sav)
+      time = int(temp[3])
+      dat = float(temp[4])
+      tM[time] += dat
 
 # Check special case that last block is full
 # Assume last few measurements are equally spaced
 if cfgs[-1] >= (begin + block_size - cfgs[-1] + cfgs[-2]):
-  Pdat.append(tP / float(count))
-  Fdat.append(tF / float(count))
-  Ddat.append(tD / float(count))
-  Rdat.append(tR / float(count))
+  for t in range(Npts - 1):
+    effm[t].append(np.log(tM[t + 1] / tM[t]))
   # Record block data
   block_data[0].append(count)
   block_data[1].append(begin)
   block_data[2].append(begin + block_size)
 
-Nblocks = len(Pdat)
+Nblocks = len(effm[0])
 # ------------------------------------------------------------------
 
 
@@ -133,29 +124,13 @@ if Nblocks == 1:
   sys.exit(1)
 
 print "Averaging with %d blocks of length %d MDTU" % (Nblocks, block_size)
-outfile = open('results/flavor.dat', 'w')
+outfile = open('results/sugra_effm.dat', 'w')
 print >> outfile, "# Averaging with %d blocks of length %d MDTU" % (Nblocks, block_size)
-print >> outfile, "# diff err rel err flavor err plaq err"
-
-dat = np.array(Ddat)
-ave = np.mean(dat, dtype = np.float64)
-err = np.std(dat, dtype = np.float64) / np.sqrt(Nblocks - 1.)
-print >> outfile, "%.6g %.4g" % (ave, err),
-
-dat = np.array(Rdat)
-ave = np.mean(dat, dtype = np.float64)
-err = np.std(dat, dtype = np.float64) / np.sqrt(Nblocks - 1.)
-print >> outfile, "%.6g %.4g" % (ave, err),
-
-dat = np.array(Fdat)
-ave = np.mean(dat, dtype = np.float64)
-err = np.std(dat, dtype = np.float64) / np.sqrt(Nblocks - 1.)
-print >> outfile, "%.6g %.4g" % (ave, err),
-
-dat = np.array(Pdat)
-ave = np.mean(dat, dtype = np.float64)
-err = np.std(dat, dtype = np.float64) / np.sqrt(Nblocks - 1.)
-print >> outfile, "%.6g %.4g # %d" % (ave, err, Nblocks)
+for t in range(Npts - 1):
+  dat = np.array(effm[t])
+  ave = np.mean(dat, dtype = np.float64)
+  err = np.std(dat, dtype = np.float64) / np.sqrt(Nblocks - 1.)
+  print >> outfile, "%d %.6g %.4g # %d" % (t, ave, err, Nblocks)
 
 # More detailed block information
 #for i in range(Nblocks):
