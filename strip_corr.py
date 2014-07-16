75893da6b3b70a8eb2f233b92df8c8cef86ce9d4
David Schaich <dschaich@syr.edu>
2014-07-16 15:36:38 -0400

    Add helper utility to clean up existing output files

---
+++ strip_corr.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+import os
+import sys
+import glob
+from subprocess import call
+# ------------------------------------------------------------------
+# Strip cheap and in some cases incorrect results out of existing
+# (unsmeared) measurement files
+# Currently strips the following list of tags:
+tags = ['KONISHI ', 'SUGRA ', 'd_correlator_r: ', 'CORR_', 'rsymm: ', \
+        'INVLINK ', 'RSYMM ', 'hvy_pot', 'PLOT_LOOP ', 'DL_LOOP ', \
+        'PLOLAR_LOOP ', 'Fixing ', 'GFIX ', 'BEFORE ', 'AFTER ', \
+        'POT_LOOP ', 'D_LOOP ', 'POLAR_LOOP ']
+
+# First make sure we're calling this from the right place
+if not os.path.isdir('Out'):
+  print "ERROR: Out/ does not exist"
+  sys.exit(1)
+
+# Cycle over measurement files
+for filename in glob.glob('Out/corr.*'):
+  outfile = open('TEMP', 'w')
+  for line in open(filename):
+    toPrint = 1
+    for tag in tags:
+      if line.startswith(tag):
+        toPrint = -1
+    if toPrint > 0:
+      print >> outfile, line.rstrip()
+  outfile.close()
+  os.rename('TEMP', filename)
+# ------------------------------------------------------------------
+
