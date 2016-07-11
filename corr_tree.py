af0031401d00015f05109842149d6fe06405bfe3
daschaich <dschaich@syr.edu>
2016-07-11 17:05:25 -0400

    Checkpoint simplified discrete FT with various checks

--- corr_tree.py
+++ corr_tree.py
@@ -2,6 +2,7 @@
 import sys
 import time
 from numpy import sqrt, sin, pi, dot, exp
+from numpy.linalg import inv, det
 # ------------------------------------------------------------------
 # Print r_I (as defined in notes)
 # corresponding to input integer displacement four-vector
@@ -9,34 +10,44 @@ from numpy import sqrt, sin, pi, dot, exp
 
 # Parse arguments: Four-component integer vector and L
 if len(sys.argv) < 6:
-  print "Usage:", str(sys.argv[0]), "<n_x> <n_y> <n_z> <n_t> <L>"
+  print "Usage:", str(sys.argv[0]), "<n_1> <n_2> <n_3> <n_4> <L>"
   sys.exit(1)
-n_x = int(sys.argv[1])
-n_y = int(sys.argv[2])
-n_z = int(sys.argv[3])
-n_t = int(sys.argv[4])
+n = [0, 0, 0, 0]
+n[0] = int(sys.argv[1])
+n[1] = int(sys.argv[2])
+n[2] = int(sys.argv[3])
+n[3] = int(sys.argv[4])
 L = int(sys.argv[5])
 runtime = -time.time()
 
-# Summation range will be -L / 2 + 1, ..., L / 2 inclusive
-if not L % 2 == 0:
-  print "Error: Need even L rather than", L
-  sys.exit(1)
-low  = 1 - L / 2
-high = L / 2 + 1    # Account for range() dropping upper limit
+# r- and k-space basis vectors as 4x4 matrices
+# sqrt(4/5) is the magnitude of the lattice spacing |a|
+invSqrt2  = 1.0 / sqrt(2.0)
+invSqrt6  = 1.0 / sqrt(6.0)
+invSqrt12 = 1.0 / sqrt(12.0)
+invSqrt20 = 1.0 / sqrt(20.0)
+pi_ov_L   = pi / float(L)
+twopiOvL  = 2.0j * pi / float(L)
+ehat = [[invSqrt2,        invSqrt6,        invSqrt12,        invSqrt20], \
+        [-1.0 * invSqrt2, invSqrt6,        invSqrt12,        invSqrt20], \
+        [0.0,             -2.0 * invSqrt6, invSqrt12,        invSqrt20], \
+        [0.0,             0.0,             -3.0 * invSqrt12, invSqrt20]]
+ehat = [[-1.0 * invSqrt2, invSqrt6,        invSqrt12,        invSqrt20], \
+        [0.0,             -2.0 * invSqrt6, invSqrt12,        invSqrt20], \
+        [0.0,             0.0,             -3.0 * invSqrt12, invSqrt20], \
+        [0.0,             0.0,             0.0,              -4.0 * invSqrt20]]
+ghat = (inv(ehat)).T
+ghat *= 2.0 * pi / float(L)
 
-# For later convenience
-invSqrt2  = 2.0 * pi / float(L * sqrt(2.0))
-invSqrt6  = 2.0 * pi / float(L * sqrt(6.0))
-invSqrt12 = 2.0 * pi / float(L * sqrt(12.0))
-halfSqrt5 = pi * sqrt(5.0) / float(L)
+# Convert n_i to r_i using ehat
+r = [0.0, 0.0, 0.0, 0.0]
+for i in range(4):
+  for j in range(4):
+    r[j] += n[i] * ehat[i][j]
 
-# Convert n_i to r_i using usual ehat basis
-tag = [n_x - n_y,                 n_x + n_y - 2 * n_z,
-       n_x + n_y + n_z - 3 * n_t, n_x + n_y + n_z + n_t]
+tag = [n[0] - n[1],                   n[0] + n[1] - 2 * n[2],
+       n[0] + n[1] + n[2] - 3 * n[3], n[0] + n[1] + n[2] + n[3]]
 print("tag: %d, %d, %d, %d" % (tag[0], tag[1], tag[2], tag[3]))
-r = [tag[0] / sqrt(2.0),  tag[1] / sqrt(6.0),
-     tag[2] / sqrt(12.0), tag[3] / sqrt(20.0)]
 mag = sqrt(dot(r, r))
 print "|r| = %.4g" % mag
 # ------------------------------------------------------------------
@@ -44,39 +55,48 @@ print "|r| = %.4g" % mag
 
 
 # ------------------------------------------------------------------
-# Integrate over (p1, p2, p3), each from 0 to L-1,
-# except for (0, 0, 0), which must be treated separately
+# Integrate over (p1, p2, p3, p4), each from 0 to L-1,
+# except for (0, 0, 0, 0), which must be treated separately
 # Be lazy and re-compute (almost) everything within the lowest-level loop
 toSum = 0.0 + 0.0j
-for p1 in range(low, high):
-  for p2 in range(low, high):
-    for p3 in range(low, high):
-      for p4 in range(low, high):
+k = [0.0, 0.0, 0.0, 0.0]
+for p1 in range(L):
+  for p2 in range(L):
+    for p3 in range(L):
+      for p4 in range(L):
         # Omit zero-mode contribution
         # TODO: Separate analytical computation possible?
         if p1 == 0 and p2 == 0 and p3 == 0 and p4 == 0:
           continue
 
-        # Convert p_i to k_i using ghat basis
-        # Pattern resembles tag above, now with 2pi factors
-        k = [0.0, 0.0, 0.0, 0.0]
-        k[0] = (p1 - p2) * invSqrt2
-        k[1] = (p1 + p2 - 2.0 * p3) * invSqrt6
-        k[2] = (p1 + p2 + p3 - 3.0 * p4) * invSqrt12
-        k[3] = (p1 + p2 + p3 + p4) * halfSqrt5
+#        p1 -= L
+
+        # Convert p_i to k_i using 2pi * ghat basis
+        k[0] = p1 * ghat[0][0] + p2 * ghat[1][0] \
+             + p3 * ghat[2][0] + p4 * ghat[3][0]
+        k[1] = p1 * ghat[0][1] + p2 * ghat[1][1] \
+             + p3 * ghat[2][1] + p4 * ghat[3][1]
+        k[2] = p1 * ghat[0][2] + p2 * ghat[1][2] \
+             + p3 * ghat[2][2] + p4 * ghat[3][2]
+        k[3] = p1 * ghat[0][3] + p2 * ghat[1][3] \
+             + p3 * ghat[2][3] + p4 * ghat[3][3]
+
+        khat = [0.0, 0.0, 0.0, 0.0]
+        for a in range(4):
+          for b in range(4):
+            khat[a] += 0.5 * k[b] * ehat[a][b]
 
         # Accumulate exp(i r.k) / khatSq (overall factor of 4 outside loop)
-        num = exp(1.0j * (r[0] * k[0] + r[1] * k[1] + r[2] * k[2] \
-                                                    + r[3] * k[3]))
-        denom = (sin(0.5 * k[0]))**2 + (sin(0.5 * k[1]))**2 \
-              + (sin(0.5 * k[2]))**2 + (sin(0.5 * k[3]))**2
+        num = exp(twopiOvL * (n[0] * p1 + n[1] * p2 + n[2] * p3 + n[3] * p4))
+        denom = (sin(khat[0]))**2 + (sin(khat[1]))**2 \
+              + (sin(khat[2]))**2 + (sin(khat[3]))**2
+#        denom = (sin(pi_ov_L * p1))**2 + (sin(pi_ov_L * p2))**2 \
+#              + (sin(pi_ov_L * p3))**2 + (sin(pi_ov_L * p4))**2
         toSum += num / denom
-#        print "%d %d %d --> %.4g / %.4g = %.4g" \
-#              % (p1, p2, p3, num, khatSq, num / khatSq)
 
 # Overall square root and constant factor of sqrt(4pi^2 / 4L^4),
 # Determinants now cancel, so include only factor of 2 for trace normalization
-one_ov_rI = pi * sqrt(2.0 * toSum) / float(L**2)
+one_ov_rI = pi * sqrt(toSum) / float(L**2)
 
 # Print along with r_I itself
 rI = 1.0 / (one_ov_rI)
