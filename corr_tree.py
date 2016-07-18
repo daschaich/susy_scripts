f7e9f243e51e6afeb5b9202029f88692e4336df8
daschaich <dschaich@syr.edu>
2016-07-18 15:54:25 -0400

    Generalize finite-volume Fourier transform to handle extended temporal lengths

--- corr_tree.py
+++ corr_tree.py
@@ -7,9 +7,9 @@ from numpy import sqrt, sin, exp, pi, dot
 # corresponding to input integer displacement four-vector
 # Use finite-volume discrete Fourier transform
 
-# Parse arguments: Four-component integer vector and L
-if len(sys.argv) < 6:
-  print "Usage:", str(sys.argv[0]), "<n_1> <n_2> <n_3> <n_4> <L>"
+# Parse arguments: Four-component integer vector, L and Nt
+if len(sys.argv) < 7:
+  print "Usage:", str(sys.argv[0]), "<n_1> <n_2> <n_3> <n_4> <L> <Nt>"
   sys.exit(1)
 n = [0, 0, 0, 0]
 n[0] = int(sys.argv[1])
@@ -17,6 +17,7 @@ n[1] = int(sys.argv[2])
 n[2] = int(sys.argv[3])
 n[3] = int(sys.argv[4])
 L = int(sys.argv[5])
+Nt = int(sys.argv[6])
 runtime = -time.time()
 
 # A4* basis vectors as 4x4 matrix
@@ -26,6 +27,8 @@ invSqrt12 = 1.0 / sqrt(12.0)
 invSqrt20 = 1.0 / sqrt(20.0)
 pi_ov_L   = pi / float(L)
 twopiOvL  = 2.0j * pi / float(L)
+pi_ov_Nt  = pi / float(Nt)
+twopiOvNt = 2.0j * pi / float(Nt)
 ehat = [[invSqrt2,        invSqrt6,        invSqrt12,        invSqrt20], \
         [-1.0 * invSqrt2, invSqrt6,        invSqrt12,        invSqrt20], \
         [0.0,             -2.0 * invSqrt6, invSqrt12,        invSqrt20], \
@@ -51,21 +54,22 @@ toSum = 0.0 + 0.0j
 for p1 in range(L):
   for p2 in range(L):
     for p3 in range(L):
-      for p4 in range(L):
+      for p4 in range(Nt):
         # Omit zero-mode contribution
         # TODO: Separate analytical computation possible?
         if p1 + p2 + p3 + p4 == 0:
           continue
 
         # Accumulate exp(i r.k) / khatSq (overall factor of 4 outside loop)
-        num = exp(twopiOvL * (n[0] * p1 + n[1] * p2 + n[2] * p3 + n[3] * p4))
+        num = exp(twopiOvL * (n[0] * p1 + n[1] * p2 + n[2] * p3)
+                + twopiOvNt * n[3] * p4)
         denom = (sin(pi_ov_L * p1))**2 + (sin(pi_ov_L * p2))**2 \
-              + (sin(pi_ov_L * p3))**2 + (sin(pi_ov_L * p4))**2
+              + (sin(pi_ov_L * p3))**2 + (sin(pi_ov_Nt * p4))**2
         toSum += num / denom
 
-# Overall square root and constant factor of sqrt(4pi^2 / 4L^4),
+# Overall square root and constant factor of sqrt(4pi^2 / 4V),
 # Don't add any extra normalization (determinants cancel)
-one_ov_rI = pi * sqrt(toSum) / float(L**2)
+one_ov_rI = pi * sqrt(toSum / float(L**3 * Nt))
 
 # Print along with r_I itself
 rI = 1.0 / (one_ov_rI)
