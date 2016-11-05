881a68293d0010c64628da25c344bbce4dd9a36a
daschaich <daschaich@gmail.com>
2016-11-05 18:10:20 +0100

    Add Cuba evaluation of r_I

---
+++ Cuba/corr_cuba.c
@@ -0,0 +1,108 @@
+// -----------------------------------------------------------------
+// Print r_I (as defined in notes)
+// corresponding to input integer displacement four-vector
+// Use vegas to numerically calculate the infinite-volume Fourier transform
+// Includes and definitions
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "cuba.h"
+
+#define    PI 3.141592653589793
+#define TWOPI 6.283185307179586
+#define  PISQ 9.869604401089358
+
+// Double precision wall clock time in seconds
+#include <sys/time.h>
+double dclock() {
+  struct timeval tp;
+  gettimeofday(&tp, NULL);
+  return ((double)tp.tv_sec + (double)tp.tv_usec * 1.e-6);
+}
+// An alternative if sys/time.h isn't available
+//double dclock() {
+//  return (((double)clock()) / CLOCKS_PER_SEC);
+//}
+// -----------------------------------------------------------------
+
+
+
+// -----------------------------------------------------------------
+// Function to integrate -- only want real part of exp
+// Delay sqrt until end -- negative evaluations seem possible
+// Don't add any extra normalization (determinants cancel)
+// Integrating over dp = dk / (2pi) removes 2pi factors from measure
+static int f(const int *ndim, const cubareal *p,
+             const int *ncomp, cubareal *ff, void *userdata) {
+
+  int nx, ny, nz, nt;
+  cubareal tr;
+
+  // Unpack userdata
+  nx = ((int*)userdata)[0];
+  ny = ((int*)userdata)[1];
+  nz = ((int*)userdata)[2];
+  nt = ((int*)userdata)[3];
+
+  // Evaluate integrand
+  tr = nx * p[0] + ny * p[1] + nz * p[2] + nt * p[3];
+  *ff = PISQ * cos(TWOPI * tr);
+  tr = sin(PI * p[0]) * sin(PI * p[0]) + sin(PI * p[1]) * sin(PI * p[1])
+     + sin(PI * p[2]) * sin(PI * p[2]) + sin(PI * p[3]) * sin(PI * p[3]);
+  *ff /= tr;
+  return 0;
+}
+// -----------------------------------------------------------------
+
+
+
+// -----------------------------------------------------------------
+int main(int argc, char *argv[]) {
+  int i, n[4], fail;
+  long long int neval;
+  cubareal integral, error, prob;
+  double abserr, dtime, val, err;
+
+  // Read in command-line arguments
+  // abserr is absolute error on the integral = 1 / r_I^2
+  // It may need to be adjusted to obtain the desired r_I accuracy
+  if (argc < 6) {
+    printf("%s <nx> <ny> <nz> <nt> <error>\n", argv[0]);
+    return -1;
+  }
+  for (i = 0; i < 4; i++)
+    n[i] = atoi(argv[i + 1]);
+  abserr = atof(argv[5]);
+
+  // Arguments: NDIM, NCOMP, function to integrate, user data, nvec
+  //            EPSREL, EPSABS, VERBOSE, SEED
+  //            MINEVAL, MAXEVAL, NSTART, NINCREASE, NBATCH
+  //            GRIDNO, STATEFILE, SPIN
+  dtime = -dclock();
+  llVegas(4, 1, f, (void*)n, 1,
+        1e-4, abserr, 2, 0,
+        0, 1e12, 1e5, 1e5, 1000,    // Need long long ints!
+        0, NULL, NULL,
+        &neval, &fail, &integral, &error, &prob);
+
+  // Have checked that 1 - prob matches confidence level
+  // from chi^2 and dof reported after the final iteration
+  if (fail == 0)
+    printf("\n\nSuccess after %dM evalutions\n", (int)(neval / 1e6));
+  else
+    printf("\n\nFailure after %dM evalutions\n", (int)(neval / 1e6));
+  printf("result = %.8g %.4g with Q = %.2g\n",
+         (double)integral, (double)error, 1 - (double)prob);
+
+  // Print r_I itself with propagated uncertainty
+  //  delta(1 / sqrt(r)) = delta(r) / 2r^(3 / 2)
+  val = 1.0 / sqrt(integral);
+  err = 0.5 * error * val * val * val;
+  printf("--> r_I = %.8g %.4g\n", val, err);
+
+  dtime += dclock();
+  printf("\nTime = %.4g seconds\n", dtime);
+  fflush(stdout);
+  return 0;
+}
+// -----------------------------------------------------------------
