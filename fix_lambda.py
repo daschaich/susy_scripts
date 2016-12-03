2afcca0ed0c640ee70131a19077103820c1043ca
daschaich <daschaich@gmail.com>
2016-12-03 16:29:07 +0100

    Add truncated 2d convention-fixing script

---
+++ fix_lambda.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+import os
+import sys
+import glob
+import math
+from subprocess import call
+# ------------------------------------------------------------------
+# Impose consistent conventions for lambda and error_per_site,
+# which were temporarily redefined for "slnc_code_2" computations
+
+# First make sure we're calling this from the right place
+if not os.path.isdir('Out'):
+  print "ERROR: Out/ does not exist"
+  sys.exit(1)
+
+# Extract Nc from path -- it's after 'Nc' then before '_'
+cwd = os.getcwd()
+temp = (cwd.split('Nc'))[1]
+Nc = float((temp.split('_'))[0])
+
+# Cycle over all output files (including eig, phase, etc)
+for filename in glob.glob('Out/*'):
+  outfile = open('TEMP', 'w')
+  check = -1
+  for line in open(filename):
+    # Check for slnc_code vs. slnc_code_2
+    if 'slnc_code' in line:
+      if 'slnc_code_2' in line:   # Good to go
+        check = 1
+      else:                       # Move to next file
+        break
+
+    if line.startswith('lambda '):
+      continue
+    elif line.startswith('error_per_site '):
+      err = math.sqrt(float((line.split())[1]))
+      print >> outfile, "error_per_site %g" % err
+    elif line.startswith('lambda='):
+      temp = line.rstrip()    # Extract kappa from end of line
+      ka = float((temp.split('='))[-1])
+      la = 0.5 * Nc / ka
+      print >> outfile, "kappa=Nc/(2lambda)=%g --> lambda=%g" % (ka, la)
+    else:
+      print >> outfile, line.rstrip()
+
+  outfile.close()
+  if check > 0:
+    os.rename('TEMP', filename)
+  else:
+    os.remove('TEMP', filename)
+# ------------------------------------------------------------------
+
