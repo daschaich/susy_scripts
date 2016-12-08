a722eeac8d37fcce35cae86b35438ec922430144
daschaich <daschaich@gmail.com>
2016-12-08 21:16:46 +0100

    Give convenience script more convenient formatting

--- check_gamma.py
+++ check_gamma.py
@@ -20,23 +20,33 @@ if not os.path.isdir('Out'):
 # Grab last output file
 infile = (sorted(glob.glob('Out/out.*')))[-1]
 for line in open(infile):
-  if line.startswith('nt '):
+  if line.startswith('nz '):
+    L = float((line.split())[-1])
+  elif line.startswith('nt '):
     Nt = float((line.split())[-1])
-  if line.startswith('bmass '):
+  elif line.startswith('lambda '):
+    la = float((line.split())[-1])
+  elif line.startswith('bmass '):
     mu = float((line.split())[-1])
   elif line.startswith('kappa='):
     temp = line.rstrip()    # Extract lambda from end of line
     la = float((temp.split('='))[-1])
 
-print "lambda = %g" % la
-print "mu = %g" % mu
+rx = rt * L / Nt
+#print "rt, rx = %g, %g" % (rt, rx)
+#print "lambda = %g" % la
+#print "mu = %g" % mu
 
 ga = mu / math.sqrt(la)
 check = mu * Nt / rt
-print "gamma = %.4g = %.4g" % (ga, check)
+#print "gamma = %.4g = %.4g" % (ga, check)
 
 combo = (mu * Nt)**2 / la
 check = (Nt * ga)**2
-print "scale = %.4g = %.4g" % (combo, check)
+#print "scale = %.4g = %.4g" % (combo, check)
+
+# Format for tex tables...
+print "%.3f & %.3f & $%d\X %d$  & %.7f & %.7f & %.2f                                              & %.2f" \
+      % (rt, rx, int(L), int(Nt), la, mu, ga, combo)
 # ------------------------------------------------------------------
 
