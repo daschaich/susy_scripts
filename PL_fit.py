dc7c0c5bfabbce40f1b0507b3bd928783bbcdd6e
daschaich <daschaich@gmail.com>
2018-11-14 21:47:47 +0100

    Replace legacy wrapper with current scipy least_squares routine

--- PL_fit.py
+++ PL_fit.py
@@ -5,10 +5,10 @@ import numpy as np
 from scipy.optimize import least_squares
 from scipy.special import gammainc
 # ------------------------------------------------------------------
-# Fit 2d N=(8,8) SYM unitarized Wilson line data to
-#   WL = A / (1 + exp[B * (r_beta - C)]) + D
+# Fit BMN Polyakov loop data to
+#   PL = D - A / (1 + exp[B * (T/mu - C)])
 
-# Parse argument: the file to analyze (FORMAT: r_beta dat err)
+# Parse argument: the file to analyze (FORMAT: T/mu dat err)
 if len(sys.argv) < 2:
   print("Usage:", str(sys.argv[0]), "<file>")
   sys.exit(1)
@@ -20,21 +20,21 @@ if not os.path.isfile(filename):
 
 # errfunc will be minimized via least-squares optimization
 # p_in are order-of-magnitude initial guesses
-expfunc = lambda p, x: p[0] / (1.0 + np.exp(p[1] * (x - p[2]))) + p[3]
+expfunc = lambda p, x: p[0] - p[1] / (1.0 + np.exp(p[2] * (x - p[3])))
 errfunc = lambda p, x, y, err: (expfunc(p, x) - y) / err
-p_in = np.array([1, 100, 0.1, 0.1])
+p_in = np.array([0.9, 0.9, 100.0, 0.1])
 lower = np.array([0.0, 0.0, 0.0, 0.0])
-upper = np.array([1.0, np.inf, 1.0, 1.0])
+upper = np.array([1.0, 1.0, np.inf, 1.0])
 
 # Define corresponding Jacobian matrix
 def jac(p, x, y, err):
   J = np.empty((x.size, p.size), dtype = np.float)
-  num = np.exp(p[1] * (x - p[2]))
-  den = 1.0 + np.exp(p[1] * (x - p[2]))
-  J[:, 0] = 1.0 / den
-  J[:, 1] = -1.0 * p[0] * (x - p[2]) * num / den**2
-  J[:, 2] = p[0] * p[1] * num / den**2
-  J[:, 3] = 1.0
+  num = np.exp(p[2] * (x - p[3]))
+  den = 1.0 + np.exp(p[2] * (x - p[3]))
+  J[:, 0] = 1.0
+  J[:, 1] = -1.0 / den
+  J[:, 2] = p[1] * (x - p[3]) * num / den**2
+  J[:, 3] = -1.0 * p[1] * p[2] * num / den**2
   for i in range(p.size):
     J[:, i] /= err
   return J
@@ -44,23 +44,23 @@ def jac(p, x, y, err):
 
 # ------------------------------------------------------------------
 # Read, parse and fit data
-# Assumed format: r_beta dat err
-rList = []
+# Assumed format: T/mu dat err
+TList = []
 datList = []
 errList = []
 for line in open(filename):
   if len(line) == 1 or line.startswith('#') or line.startswith('!'):
     continue
   temp = line.split()
-  rList.append(float(temp[0]))
+  TList.append(float(temp[0]))
   datList.append(float(temp[1]))
   errList.append(float(temp[2]))
-r = np.array(rList)
+T = np.array(TList)
 dat = np.array(datList)
 err = np.array(errList)
 
 # For now, demand that we have degrees of freedom
-dof = len(r) - 4
+dof = len(T) - 4
 if dof < 1:
   print("ERROR: dof > 0 required for now")
   sys.exit(1)
@@ -69,7 +69,7 @@ if dof < 1:
 # and covariance matrix (J^T J)^{-1} where J is jacobian matrix
 # Levenberg--Marquardt (method='lm') can't handle bounds
 all_out = least_squares(errfunc, p_in, bounds=[lower, upper],
-                        jac=jac, method='trf', args=(r, dat, err))
+                        jac=jac, method='trf', args=(T, dat, err))
 p_out = all_out.x
 tj = all_out.jac
 covar = np.linalg.inv(np.dot(np.transpose(tj), tj))
@@ -79,23 +79,23 @@ if all_out.success < 0 or all_out.success > 4:
   print(errmsg)
   sys.exit(1)
 
-# Error propagation for high-temperature (small-r) limit
-hiT = p_out[0] + p_out[3]
-derivs = np.array([1.0, 0.0, 0.0, 1.0])
-hiTerr = np.sqrt(np.dot(derivs, np.dot(covar, derivs)))
+# Error propagation for low-temperature limit
+loT = p_out[0] - p_out[1]
+derivs = np.array([1.0, -1.0, 0.0, 0.0])
+loTerr = np.sqrt(np.dot(derivs, np.dot(covar, derivs)))
 
-print("Critical r_beta: %.6g %.4g" % (p_out[2], np.sqrt(covar[2][2])))
-print("High-temp limit: %.6g %.4g" % (hiT, hiTerr))
-print(" Low-temp limit: %.6g %.4g" % (p_out[3], np.sqrt(covar[3][3])))
-print("    'Steepness': %.6g %.4g" % (p_out[1], np.sqrt(covar[1][1])))
+print("Critical r_beta: %.6g %.4g" % (p_out[3], np.sqrt(covar[3][3])))
+print("High-temp limit: %.6g %.4g" % (p_out[0], np.sqrt(covar[0][0])))
+print(" Low-temp limit: %.6g %.4g" % (loT, loTerr))
+print("    'Steepness': %.6g %.4g" % (p_out[2], np.sqrt(covar[2][2])))
 
 # Compute chiSq and confidence level of fit
-chiSq = ((errfunc(p_out, r, dat, err))**2).sum()
+chiSq = ((errfunc(p_out, T, dat, err))**2).sum()
 CL = 1.0 - gammainc(0.5 * dof, 0.5 * chiSq)
 print("chiSq/dof = %.4g/%d = %.4g --> CL = %.4g" \
       % (chiSq, dof, chiSq / dof, CL))
 
 # Format to copy+paste into gnuplot
-#print("\nf(x)=%.4g / (1 + exp(%.4g * (x - %.4g))) + %.4g" \
+#print("\nf(x)=%.4g - %.4g / (1 + exp(%.4g * (x - %.4g)))" \
 #      % (p_out[0], p_out[1], p_out[2], p_out[3]))
 # ------------------------------------------------------------------
