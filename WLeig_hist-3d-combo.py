1aa93f08dde66b2cb255a56468d2c235dcc0868f
daschaich <daschaich@gmail.com>
2020-08-31 22:10:31 +0100

    Change 3d WLeig histogram plotting to include N dependence

--- WLeig_hist-3d-combo.py
+++ WLeig_hist-3d-combo.py
@@ -7,7 +7,9 @@ import matplotlib.pyplot as plt
 # ------------------------------------------------------------------
 # Plot histogram of Wilson line eigenvalues
 # Use data/WLeig.csv file for time-series plots
-# Superimpose Ndirs=3 ensembles specified by input arguments
+# Superimpose five ensembles specified by input arguments
+#   First three (filled) are N=4, 6 and 8 with L=12
+#   Last two (unfilled) are L=8 and 16 with N=8
 # For now only consider unitarized (and not full) Wilson lines in z-dir
 # !!! Assume only thermalized measurements have been run
 # Save resulting plot as ./WLeig_hist.pdf
@@ -15,13 +17,14 @@ import matplotlib.pyplot as plt
 # Parse arguments: L=8, 12 and 16 ensembles,
 # the upper bound for the y-axis,
 # and finally a tag for the title for the plot
-if len(sys.argv) < 6:
+if len(sys.argv) < 8:
   print("Usage:", str(sys.argv[0]), end='')
   print("<U(4) dir> <U(6) dir> <U(8) dir>", end='')
+  print("<L=8 dir> <L=16 dir>", end='')
   print("<y-axis upper bound> <plot title tag>")
   sys.exit(1)
 
-Ndirs = 3
+Ndirs = 5
 dirnames = []
 for i in range(Ndirs):
   dirnames.append(str(sys.argv[i + 1]))
@@ -57,11 +60,15 @@ for dirname in dirnames:
 nbins = 10
 plt.figure(figsize=(6.40, 3.84))    # Gnuplot default
 plt.hist(dat[0], nbins, log=False, density=True, align='mid',
-         edgecolor='red', label='N=4', histtype='step', hatch='||')
+         edgecolor='red', label='N=4, L=12', histtype='step', hatch='||')
 plt.hist(dat[1], nbins, log=False, density=True, align='mid',
-         edgecolor='green', label='N=6', histtype='step', hatch='\\\\')
+         edgecolor='green', label='N=6, L=12', histtype='step', hatch='\\\\')
 plt.hist(dat[2], nbins, log=False, density=True, align='mid',
-         edgecolor='blue', label='N=8', histtype='step', hatch='//')
+         edgecolor='blue', label='N=8, L=12', histtype='step', hatch='//')
+plt.hist(dat[3], nbins, log=False, density=True, align='mid',
+         edgecolor='orange', label='N=8, L=8', histtype='step')
+plt.hist(dat[4], nbins, log=False, density=True, align='mid',
+         edgecolor='purple', label='N=8, L=16', histtype='step')
 
 plt.axis([-np.pi, np.pi, 0.0, ymax])
 plt.xticks([-np.pi, -0.5 * np.pi, 0.0, 0.5 * np.pi, np.pi],
