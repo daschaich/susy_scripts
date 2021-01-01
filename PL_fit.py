6369305332275341e947dddd5e1bd7b9225a932c
daschaich <daschaich@gmail.com>
2021-01-01 17:28:43 +0000

    Revert to remove bounds and restore consistency with jacobian

--- PL_fit.py
+++ PL_fit.py
@@ -24,12 +24,9 @@ if not os.path.isfile(filename):
 
 # errfunc will be minimized via least-squares optimization
 # p_in are order-of-magnitude initial guesses
-# Lower and upper are constant bounds
-expfunc = lambda p, x: p[0] * (1.0 - p[1] / (1.0 + np.exp(p[2] * (x - p[3]))))
+expfunc = lambda p, x: p[0] - p[1] / (1.0 + np.exp(p[2] * (x - p[3])))
 errfunc = lambda p, x, y, err: (expfunc(p, x) - y) / err
 p_in = np.array([0.9, 0.9, 100.0, 0.1])
-#lower = np.array([0.0, 0.0, 0.0, 0.0])
-#upper = np.array([1.0, 1.0, np.inf, 1.0])
 
 # Define corresponding Jacobian matrix
 def jac(p, x, y, err):
@@ -76,8 +73,7 @@ if dof < 1:
 # Extract and save fit parameters
 # and covariance matrix (J^T J)^{-1} where J is jacobian matrix
 # Levenberg--Marquardt (method='lm') can't handle bounds
-all_out = least_squares(errfunc, p_in, #bounds=[lower, upper],
-                        jac=jac, method='trf', args=(T, dat, err))
+all_out = least_squares(errfunc, p_in, jac=jac, args=(T, dat, err))
 p_out = all_out.x
 tj = all_out.jac
 cov = np.linalg.inv(np.dot(np.transpose(tj), tj))
@@ -88,8 +84,8 @@ if all_out.success < 0 or all_out.success > 4:
   sys.exit(1)
 
 # Error propagation for low-temperature limit
-loT = p_out[0] * (1.0 - p_out[1])
-derivs = np.array([1.0 - p_out[1], -p_out[0], 0.0, 0.0])
+loT = p_out[0] - p_out[1]
+derivs = np.array([1.0, -1.0, 0.0, 0.0])
 loTerr = np.sqrt(np.dot(derivs, np.dot(cov, derivs)))
 
 print("  Critical T/mu: %.6g %.4g" % (p_out[3], np.sqrt(cov[3][3])))
@@ -104,6 +100,6 @@ print("chiSq/dof = %.4g/%d = %.4g --> CL = %.4g" \
       % (chiSq, dof, chiSq / dof, CL))
 
 # Format to copy+paste into gnuplot
-print("\nf(x)=%.4g * (1 - %.4g / (1 + exp(%.4g * (x - %.4g))))" \
+print("\nf(x)=%.4g - %.4g / (1 + exp(%.4g * (x - %.4g)))" \
       % (p_out[0], p_out[1], p_out[2], p_out[3]))
 # ------------------------------------------------------------------
